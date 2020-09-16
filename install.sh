cf968fbc31b91f8a4b2716bbc3de7f32b91e4b13
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2020-09-16 14:50:50 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,4 @@ GO111MODULE=auto go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 GO111MODULE=auto go get -u -v github.com/projectdiscovery/subfinder/cmd/subfinder
 apt-get install amass
 
-curl -Lo slackcat https://github.com/bcicen/slackcat/releases/download/v1.6/slackcat-1.6-$(uname -s)-amd64
-sudo mv slackcat /usr/local/bin/
-sudo chmod +x /usr/local/bin/slackcat
-
-slackcat --configure
+go get github.com/balook/teleman
